91f3a695aea1e18f0d0cedcc4de0564631be13a4
kamel-guerda <kamel.guerda@idris.fr>
2023-11-17 09:37:40 +0100

    added loader option in dataset to circumvent corrupted files

--- data_pipeline/dataset.py
+++ data_pipeline/dataset.py
@@ -1,26 +1,33 @@
 from torch.utils.data import Dataset
 import cv2
+import numpy as np
+import pandas as pd
+from PIL import Image, ImageFile
 from pathlib import Path
 
-import pandas as pd
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class LynxDataset(Dataset):
-    def __init__(self,
-                 dataset_csv: Path
-    ):
+    def __init__(self, dataset_csv: Path, loader='pil'):
         self.dataset_csv = dataset_csv
         self.dataframe = pd.read_csv(dataset_csv)
+        self.loader = loader  # 'pil' or 'opencv'
                 
     def __getitem__(self, idx):
         # Retrieve the row corresponding to the index
         image_id = self.dataframe.iloc[idx]
 
-        # Load the image
-        img = cv2.imread(image_id["filepath"])
-        try:
+        # Load the image using the specified loader
+        if self.loader == 'opencv':
+            img = cv2.imread(image_id["filepath"])
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        except:
-            img = None
+            if img is None:
+                raise ValueError(f"Image not found or corrupted at {image_id['filepath']}")
+        elif self.loader == 'pil':
+            img = Image.open(image_id["filepath"])
+            img = np.array(img.convert('RGB'))  # Convert to RGB
+        else:
+            raise ValueError("Unsupported loader. Choose 'pil' or 'opencv'.")
 
         # Prepare the input dictionary with the image and other data
         input_dict = {
@@ -39,7 +46,5 @@ class LynxDataset(Dataset):
 
         return input_dict, output_dict
         
-        
     def __len__(self):
         return len(self.dataframe)
-        
