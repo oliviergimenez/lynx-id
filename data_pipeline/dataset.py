b1632669278b944e0c9af204c90f0cdacbbb4577
kamel-guerda <kamel.guerda@idris.fr>
2023-11-17 14:12:08 +0100

    added options for transformations and augmentations in the dataset + tests in a notebook

--- data_pipeline/dataset.py
+++ data_pipeline/dataset.py
@@ -4,32 +4,43 @@ import numpy as np
 import pandas as pd
 from PIL import Image, ImageFile
 from pathlib import Path
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+ImageFile.LOAD_TRUNCATED_IMAGES = False
 
 class LynxDataset(Dataset):
-    def __init__(self, dataset_csv: Path, loader='pil'):
+    def __init__(self, dataset_csv: Path, loader='pil', transform=None, augmentation=None):
         self.dataset_csv = dataset_csv
         self.dataframe = pd.read_csv(dataset_csv)
         self.loader = loader  # 'pil' or 'opencv'
+        self.transform = transform
+        self.augmentation = augmentation
                 
     def __getitem__(self, idx):
-        # Retrieve the row corresponding to the index
         image_id = self.dataframe.iloc[idx]
 
         # Load the image using the specified loader
         if self.loader == 'opencv':
             img = cv2.imread(image_id["filepath"])
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             if img is None:
                 raise ValueError(f"Image not found or corrupted at {image_id['filepath']}")
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         elif self.loader == 'pil':
             img = Image.open(image_id["filepath"])
             img = np.array(img.convert('RGB'))  # Convert to RGB
         else:
             raise ValueError("Unsupported loader. Choose 'pil' or 'opencv'.")
 
-        # Prepare the input dictionary with the image and other data
+        # Apply transformations (e.g., resizing)
+        if self.transform:
+            img = self.transform(image=img)['image']
+
+        # Apply augmentations
+        if self.augmentation:
+            img = self.augmentation(image=img)['image']
+
+        # Prepare the input and output dictionaries
         input_dict = {
             'image': img,
             'source': image_id["source"],
@@ -39,7 +50,6 @@ class LynxDataset(Dataset):
             'image_number': image_id["image_number"]
         }
 
-        # Prepare the output dictionary with lynx_id
         output_dict = {
             'lynx_id': image_id["lynx_id"]
         }
