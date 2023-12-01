73d2ef697d6c160ac361458667718ac23d980a8a
thomasbtnfr <thomas.betton@irisa.fr>
2023-12-01 17:30:10 +0100

    MegaDetector output respecting the "coco" convention + threshold 0.5

--- data_pipeline/megadetector/utils.py
+++ data_pipeline/megadetector/utils.py
@@ -1,21 +1,22 @@
 from PIL import Image
 
 
-def crop_bbox(row_bbox):
+def crop_bbox(row_bbox, to_be_normalized=False):
     img = Image.open(row_bbox["file"])
     x, y, width, height = row_bbox[['x', 'y', 'width', 'height']]
 
-    im_width, im_height = img.size
+    if to_be_normalized:
+        im_width, im_height = img.size
 
-    x_norm = x * im_width
-    y_norm = y * im_height
-    w_norm = width * im_width
-    h_norm = height * im_height
+        x *= im_width
+        y *= im_height
+        width *= im_width
+        height *= im_height
 
-    left = x_norm
-    top = y_norm
-    right = x_norm + w_norm
-    bottom = y_norm + h_norm
+    left = x
+    top = y
+    right = x + width
+    bottom = y + height
 
     # Crop and save to visualize it
     return img.crop((left, top, right, bottom))
