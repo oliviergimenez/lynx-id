2baf941689718b2ac207a28ee04bf4c576bd67f3
thomasbtnfr <thomas.betton@irisa.fr>
2024-05-03 14:24:03 +0200

    No max-width doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,18 +18,18 @@ extensions = [
     'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',  # For Google or NumPy style docstrings,
-    'sphinx.ext.viewcode', # To include links to the library 
-    'myst_parser', # To parse md files
+    'sphinx.ext.viewcode',  # To include links to the library
+    'myst_parser',  # To parse md files
     'nbsphinx'
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
